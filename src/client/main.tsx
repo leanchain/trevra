4538185129dbc960ed070d26206232dcbaf6394e
pankaj4u4m <pankaj4u4m@gmail.com>
2026-08-13 19:34:08 +0200

    Mount LinkedIn outreach manager client root

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { ClientRoot } from './ClientRoot';
 import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClientRoot />
   </StrictMode>
 );
